c70204c85096d681dc4655523673c9d49e67a43b
coil398 <konayuta.sou@gmail.com>
2017-10-07 06:19:32 +0900

    added set.sh

---
+++ etc/set.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+OS=`uname`
+
+case "${OS}" in
+    Darwin*)
+        ;;
+    Linux*)
+        curl https://raw.githubusercontent.com/seebi/dircolors-solarized/master/dircolors.256dark -o $HOME/.zsh/dircolors-solarized/dircolors.256dark
+        mv $HOME/.linuxbrew $HOME/dotfiles/.linuxbrew
+        ;;
+esac
+
+mv $HOME/.enhancd $HOME/dotfiles/.enhancd
+mv $HOME/.pyenv $HOME/dotfiles/.pyenv
+mv $HOME/.nodenv $HOME/dotfiles/.nodenv
+mv $HOME/.rbenv $HOME/dotfiles/.rbenv
+mv $HOME/.cache $HOME/dotfiles/.cache
