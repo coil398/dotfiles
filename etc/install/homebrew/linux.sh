e75b44dc59a8fa0b6512ed98629a904609834e8c
coil398 <konayuta.sou@gmail.com>
2017-10-06 15:10:29 +0900

    apt update and upgrade

--- etc/install/homebrew/linux.sh
+++ etc/install/homebrew/linux.sh
@@ -4,13 +4,9 @@ SCRIPT_DIR=`dirname $0`
 cd $SCRIPT_DIR
 . '../../util.sh'
 
-if has "sudo"; then
-    sudo apt -y install build-essential file git python-setuptools ruby
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
-else
-    apt -y install build-essential file git python-setuptools ruby
-    yes | ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
-fi
+sudo apt -y update && sudo apt -y upgrade
+sudo apt -y install build-essential file git python-setuptools ruby
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
 export PATH="$HOME/.linuxbrew/bin:$PATH"
 export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"
 export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"
