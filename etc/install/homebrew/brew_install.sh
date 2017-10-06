d2b60a79aa576ac03d7c2e917ed2affe4156546c
coil398 <konayuta.sou@gmail.com>
2017-10-07 03:41:11 +0900

    modified

--- etc/install/homebrew/brew_install.sh
+++ etc/install/homebrew/brew_install.sh
@@ -1,15 +1,15 @@
 brew='${HOME}/.linuxbrew/bin/brew'
-brew install git
-brew install neovim
-brew install zsh
-brew install zsh-completions
-brew install zplug
-brew install pyenv
-brew install nodenv
-brew install rbenv
-brew install tmux
-brew install global --with-exuberant-ctags --with-pygments
-brew install tig
-brew install osx-cpu-temp
-brew install ag
-brew install go
+$brew install git
+$brew install neovim
+$brew install zsh
+$brew install zsh-completions
+$brew install zplug
+$brew install pyenv
+$brew install nodenv
+$brew install rbenv
+$brew install tmux
+$brew install global --with-exuberant-ctags --with-pygments
+$brew install tig
+$brew install osx-cpu-temp
+$brew install ag
+$brew install go
