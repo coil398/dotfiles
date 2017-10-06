fec4c79bd4c21e7550c2e88759dcc0cfa6f0505f
coil398 <konayuta.sou@gmail.com>
2017-10-07 03:56:49 +0900

    se-nya kawaii

--- etc/install/homebrew/brew_install.sh
+++ etc/install/homebrew/brew_install.sh
@@ -1,15 +1,14 @@
-brew='~/.linuxbrew/bin/brew'
-$brew install git
-$brew install neovim
-$brew install zsh
-$brew install zsh-completions
-$brew install zplug
-$brew install pyenv
-$brew install nodenv
-$brew install rbenv
-$brew install tmux
-$brew install global --with-exuberant-ctags --with-pygments
-$brew install tig
-$brew install osx-cpu-temp
-$brew install ag
-$brew install go
+brew install git
+brew install neovim
+brew install zsh
+brew install zsh-completions
+brew install zplug
+brew install pyenv
+brew install nodenv
+brew install rbenv
+brew install tmux
+brew install global --with-exuberant-ctags --with-pygments
+brew install tig
+brew install osx-cpu-temp
+brew install ag
+brew install go
--- etc/install/homebrew/linux.sh
+++ etc/install/homebrew/linux.sh
@@ -4,8 +4,16 @@ SCRIPT_DIR=`dirname $0`
 cd $SCRIPT_DIR
 . '../../util.sh'
 
-sudo apt -y update && sudo apt -y upgrade
-sudo apt -y install build-essential file git python-setuptools ruby
+if has 'sudo'; then
+    sudo apt -y update && sudo apt -y upgrade
+    sudo apt -y install build-essential file git python-setuptools ruby
+    sudo apt -y install linuxbrew-wrapper
+else
+    apt -y update && sudo apt -y upgrade
+    apt -y install build-essential file git python-setuptools ruby
+    apt -y install linuxbrew-wrapper
+fi
+
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
 echo 'export PATH="${HOME}/.linuxbrew/bin:$PATH"' >> $HOME/.bash_profile
 echo 'export MANPATH="${HOME}/.linuxbrew/share/man:$MANPATH"' >> $HOME/.bash_profile
