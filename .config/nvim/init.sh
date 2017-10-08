12fa186273bff9311e98b1170b84e70e7ce096e8
coil398 <konayuta.sou@gmail.com>
2017-10-08 21:01:39 +0900

    commit

--- .config/nvim/init.sh
+++ .config/nvim/init.sh
@@ -1,5 +1,4 @@
 echo 'Installing dein.nvim'
-mkdir ~/.cache
-cd ~/.cache/dein
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 sh ./installer.sh ~/.cache/dein
+rm -f installer.sh
