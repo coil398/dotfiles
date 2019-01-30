aaf07ee48000c9c6a7184d00284658e97ba9890d
coil398 <konayuta.sou@gmail.com>
2019-01-30 16:06:58 +0900

    delete installing *env packages

--- etc/install/homebrew/brew_install.sh
+++ etc/install/homebrew/brew_install.sh
@@ -4,12 +4,6 @@ brew install neovim
 brew install zsh
 brew install zsh-completions
 brew install zplug
-brew install pyenv
-brew install pyenv-virtualenv
-brew install nodenv
-brew install goenv
-brew install yarn
-brew install rbenv
 brew install tmux
 brew install reattach-to-user-namespace
 brew install tmux-mem-cpu-load
