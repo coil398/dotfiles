3222533956f2a29374005f9b898c2ca806679ba4
coil398 <konayuta.sou@gmail.com>
2023-05-04 07:42:47 +0900

    added noice

--- .config/nvim/lua/packers.lua
+++ .config/nvim/lua/packers.lua
@@ -129,6 +129,29 @@ local function init()
   }
 
   use {
+    'folke/noice.nvim',
+    config = function()
+      require('noice').setup({
+        lsp = {
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true,
+          },
+        },
+        presets = {
+          bottom_search = true,
+          command_palette = true,
+          long_message_to_split = true,
+          inc_rename = false,
+          lsp_doc_border = false
+        }
+      })
+    end,
+    requires = { 'MunifTanjim/nui.nvim', 'rcarriga/nvim-notify' }
+  }
+
+  use {
     'stevearc/aerial.nvim',
     requires = { 'nvim-tree/nvim-web-devicons' },
     config = function()
