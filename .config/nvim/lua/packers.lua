18d0a4ce450d644efc60f8381d011e01beb2e92b
coil398 <konayuta.sou@gmail.com>
2023-05-04 07:46:47 +0900

    modified noice

--- .config/nvim/lua/packers.lua
+++ .config/nvim/lua/packers.lua
@@ -141,7 +141,7 @@ local function init()
         },
         presets = {
           bottom_search = true,
-          command_palette = true,
+          command_palette = false,
           long_message_to_split = true,
           inc_rename = false,
           lsp_doc_border = false
