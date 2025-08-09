b044a051d85ce7340a6f95aeb93e3263c3819613
Takumi Kawase <t.kawase398@gmail.com>
2025-08-09 23:25:44 +0900

    fix(link.sh): make script POSIX-compliant under /bin/sh (#29)
    * fix(load.sh): use /usr/bin/env bash\n\nFixes #13
    
    * fix(zshrc): guard anyenv init and correct KREW_ROOT path\n\nFixes #14
    
    * fix(link.sh): make script POSIX-compliant under /bin/sh\n\nFixes #15

--- etc/link.sh
+++ etc/link.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
-DOT_DIRECTORY="${HOME}/dotfiles"
-cd ${DOT_DIRECTORY}
+set -eu
 
-for f in .??*
-do
-    [[ ${f} = ".git" ]] && continue
-    [[ ${f} = ".gitignore" ]] && continue
-    [[ ${f} = ".DS_Store" ]] && continue
-    ln -snfv ${DOT_DIRECTORY}/${f} ${HOME}/${f}
+DOT_DIRECTORY="${HOME}/dotfiles"
+cd "$DOT_DIRECTORY"
+
+for f in .??*; do
+    [ "$f" = ".git" ] && continue
+    [ "$f" = ".gitignore" ] && continue
+    [ "$f" = ".DS_Store" ] && continue
+    ln -snfv "$DOT_DIRECTORY/$f" "$HOME/$f"
 done
-ln -s ${DOT_DIRECTORY}/.tmux/.tmux.conf $HOME/.tmux.conf
-if [[ `uname` = "Darwin" ]];then
-    ln -s ${DOT_DIRECTORY}/.tmux/.tmux.conf.mac $HOME/.tmux.conf.mac
+
+ln -snfv "$DOT_DIRECTORY/.tmux/.tmux.conf" "$HOME/.tmux.conf"
+if [ "$(uname)" = "Darwin" ]; then
+    ln -snfv "$DOT_DIRECTORY/.tmux/.tmux.conf.mac" "$HOME/.tmux.conf.mac"
 fi
 echo 'Deploy dotfiles completed.'
