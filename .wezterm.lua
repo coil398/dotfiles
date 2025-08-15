58f1e6a2fdc81f9864855656145af64ff5296d88
coil398 <t.kawase398@gmail.com>
2025-08-16 06:55:09 +0900

    modified

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,25 +7,26 @@ config.font_size = 10.0
 config.enable_tab_bar = false
 config.use_ime = true
 config.ime_preedit_rendering = 'Builtin'
+config.copy_on_select = false
 
 local triple = wezterm.target_triple
 local is_macos = triple:find('apple%-darwin') ~= nil
 local is_windows = triple:find('windows') ~= nil
 
 if is_windows then
-	config.default_domain = 'WSL:Arch'
-	config.wsl_domains = {
-		{
-			name = 'WSL:Arch',
-			distribution = 'Arch',
-			username = 'coil398',
-			default_cwd = '/home/coil398',
-		},
-	}
+  config.default_domain = 'WSL:Arch'
+  config.wsl_domains = {
+    {
+      name = 'WSL:Arch',
+      distribution = 'Arch',
+      username = 'coil398',
+      default_cwd = '/home/coil398',
+    },
+  }
 end
 
 if is_macos then
-	config.macos_forward_to_ime_modifier_mask = 'SHIFT'
+  config.macos_forward_to_ime_modifier_mask = 'SHIFT'
 end
 
 return config
