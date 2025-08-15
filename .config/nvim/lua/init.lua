adb98cfb36c81f841f9559efd56f3b5255bf554c
coil398 <t.kawase398@gmail.com>
2025-08-16 06:55:09 +0900

    modified

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -422,6 +422,40 @@ require('lazy').setup({
         lsp_doc_border = false,
       },
     },
+    config = function()
+      require("noice").setup({
+        views = {
+          cmdline_popup = {
+            position = {
+              row = 5,
+              col = "50%",
+            },
+            size = {
+              width = 60,
+              height = "auto",
+            },
+          },
+          popupmenu = {
+            relative = "editor",
+            position = {
+              row = 8,
+              col = "50%",
+            },
+            size = {
+              width = 60,
+              height = 10,
+            },
+            border = {
+              style = "rounded",
+              padding = { 0, 1 },
+            },
+            win_options = {
+              winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+            },
+          },
+        },
+      })
+    end,
     dependencies = {
       "MunifTanjim/nui.nvim",
       "rcarriga/nvim-notify",
