106ddd4e42202cd4e1921c872569d11e28f9eca7
coil398 <t.kawase398@gmail.com>
2025-12-26 05:26:40 +0900

    fix(nvim): enable hlchunk.nvim features explicitly

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -563,7 +563,23 @@ require('lazy').setup({
     "shellRaining/hlchunk.nvim",
     event = { "BufReadPre", "BufNewFile" },
     config = function()
-      require("hlchunk").setup({})
+      require("hlchunk").setup({
+        chunk = {
+          enable = true,
+          use_treesitter = true,
+        },
+        indent = {
+          enable = true,
+          use_treesitter = true,
+        },
+        line_num = {
+          enable = true,
+          use_treesitter = true,
+        },
+        blank = {
+          enable = true,
+        }
+      })
     end
   }
 })
