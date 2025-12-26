ee74872102bafedef73128c226c9b045d6929b59
coil398 <t.kawase398@gmail.com>
2025-12-26 09:00:38 +0900

    keymappings for quickfix

--- .config/nvim/lua/keymappings.lua
+++ .config/nvim/lua/keymappings.lua
@@ -2,8 +2,23 @@
 vim.g.mapleader = " "
 
 -- Mappings for the quickfix window
-vim.keymap.set('n', '<Space>q', ':ccl<CR>')
-vim.keymap.set('n', '<Space>w', ':botright cwindow<CR>')
+local function toggle_quickfix()
+  local qf_exists = false
+  for _, win in pairs(vim.fn.getwininfo()) do
+    if win.quickfix == 1 then
+      qf_exists = true
+    end
+  end
+  if qf_exists then
+    vim.cmd('cclose')
+  else
+    vim.cmd('botright cwindow')
+  end
+end
+
+vim.keymap.set('n', '<leader>q', toggle_quickfix, { desc = 'Toggle Quickfix' })
+vim.keymap.set('n', '[q', ':cprev<CR>', { desc = 'Previous Quickfix Item' })
+vim.keymap.set('n', ']q', ':cnext<CR>', { desc = 'Next Quickfix Item' })
 
 vim.keymap.set('n', '<Space><Space>', 'i<Space><Esc>')
 
