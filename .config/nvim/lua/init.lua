91c9b1fa46f419f9933d922b4a5ecc035cf68fc6
coil398 <t.kawase398@gmail.com>
2025-12-26 09:19:09 +0900

    nvim-surround

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -84,6 +84,16 @@ require('lazy').setup({
   },
 
   {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use main branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+  },
+  {
     'scalameta/nvim-metals',
     dependencies = { "nvim-lua/plenary.nvim" }
   },
