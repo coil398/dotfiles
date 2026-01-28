a03ad4db4f674abccc5b13bd98d4d329be1f37f8
coil398 <t.kawase398@gmail.com>
2026-01-28 18:27:31 +0900

    use win32yank

--- .config/nvim/lua/linux.lua
+++ .config/nvim/lua/linux.lua
@@ -1,2 +1,20 @@
 -- Linux clipboard integration
 vim.opt.clipboard:append("unnamedplus")
+
+if vim.fn.has("wsl") == 1 then
+  local win32yank = "/mnt/c/Windows/System32/win32yank.exe"
+  if vim.fn.executable(win32yank) == 1 then
+    vim.g.clipboard = {
+      name = "win32yank-wsl",
+      copy = {
+        ["+"] = { win32yank, "-i", "--crlf" },
+        ["*"] = { win32yank, "-i", "--crlf" },
+      },
+      paste = {
+        ["+"] = { win32yank, "-o", "--lf" },
+        ["*"] = { win32yank, "-o", "--lf" },
+      },
+      cache_enabled = 0,
+    }
+  end
+end
