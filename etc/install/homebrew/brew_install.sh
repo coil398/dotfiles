a9f9ec64021b2acfc675a028856523e0af653dfe
川瀬拓実 <kawasetakumi@kawasetakuminoMac-Studio.local>
2026-04-04 18:29:53 +0900

    updated

--- etc/install/homebrew/brew_install.sh
+++ etc/install/homebrew/brew_install.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 brew update && brew upgrade
 brew install git
 brew install neovim
@@ -5,10 +7,22 @@ brew install zsh
 brew install zsh-completions
 brew install zplug
 brew install tmux
-brew install reattach-to-user-namespace
 brew install tmux-mem-cpu-load
-brew install global --with-exuberant-ctags --with-pygments
 brew install gitui
-brew install osx-cpu-temp
-brew install ag
-brew install go
+brew install ripgrep
+brew install eza
+brew install bat
+brew install procs
+brew install fd
+brew install fzf
+brew install zoxide
+brew install direnv
+brew install colordiff
+brew install tig
+brew install gh
+brew install mise
+
+# macOS 専用
+if [ "$(uname)" = "Darwin" ]; then
+    brew install reattach-to-user-namespace
+fi
